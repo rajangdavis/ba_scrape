ef69838b9d38c7631bf23a03a4aa669b11c487c3
rajangdavis <rajangdavis@gmail.com>
2018-10-09 15:47:51 -0700

    Trying to set this up on heroku

---
+++ index.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const express = require('express');
+const apicache = require('apicache');
+const Sequelize = require('sequelize')
+const env = process.env;
+
+const sequelize = new Sequelize(
+    env("POSTGRES_DB_NAME"),
+    env("POSTGRES_USER"),
+    env("POSTGRES_PASSWORD"), {
+    dialect: "postgres"
+    port: 5432
+});
+
+sequelize
+    .authenticate()
+    .then(function(err) {
+        console.log('Connection has been established successfully.');
+    }, function (err) { 
+        console.log('Unable to connect to the database:', err);
+    });
+
+apicache.options({debug:true});
+const cache = apicache.middleware; 
+const app = express();
+// app.use(cache('168 hours'));
+const server = http.createServer(app);
+  
+app.get('/', async (req, res, next) => {
+  console.log(req);
+});
+ 
+app.use(function notFoundMiddleware(req, res, next) {
+  next(Object.assign(new Error('Not Found'), { status: 404 }));
+});
+ 
+app.use((err, req, res, _next) => {
+  const { name, message, status = 500 } = err;
+  if (status >= 500) {
+    console.error(err);
+  }
+  res.status(status);
+  res.json({ name, message, status });
+});
+ 
+server.listen(process.env.PORT || 3000, () => {
+  console.info('Listening on http://localhost:3000');
+});
